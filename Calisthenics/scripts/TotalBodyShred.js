5248af26bcf7440a4063312ebbeb777d4f9e4e19
Daron Valdivia <zharkaron@BugHunter.BugHunter>
2025-10-28 20:32:10 -0500

    testing buttons for json file so i can later pull the info from the json files

---
+++ Calisthenics/scripts/TotalBodyShred.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("total body shred").addEventListener("click", () => {
+    console.log("Workout skipped!");
+    alert("You skipped this workout.");
+  });
+});
+
